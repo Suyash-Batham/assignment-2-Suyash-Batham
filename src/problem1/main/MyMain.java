c7e126207457e12dabe642dc5eb65a6344e449cb
Suyash Batham <Suyash-Batham>
2020-03-28 13:07:29 +0530

    Created Insert method and print all left child's

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -1,11 +1,26 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
- */
 package problem1.main;
-// executable class
 
-public class MyMain {
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+
+public class MyMain
+{
+    public static void main(String[] args)
+    {
+        MyBinarySearchTree mst = new MyBinarySearchTree();
+        TreeNode root;
+        root = new TreeNode(11);
+        mst.insert(root, 4);
+        mst.insert(root, 2);
+        mst.insert(root, 5);
+        mst.insert(root, 7);
+        mst.insert(root, 8);
+        mst.insert(root, 42);
+        mst.insert(root, 17);
+        mst.insert(root, 45);
+        mst.insert(root, 34);
+        mst.insert(root, 23);
+        mst.insert(root, 12);
+        mst.leftView(root);
+    }
 }
